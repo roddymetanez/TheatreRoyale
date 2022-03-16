b1f75f83438e59ce6a156e2f8cebefad34ebcfba
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2022-03-16 16:17:52 +0000

    Code format

--- src/models/Performance.java
+++ src/models/Performance.java
@@ -4,49 +4,49 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Performance {
-	
-	private int ID;
-	private int duration;
-	private Double price;
-	private LocalDateTime startDateTime;
 
-	
-	public Performance(String date, int duration) {
-		this.setDuration(duration);
-		this.startDateTime = LocalDateTime.parse(date);
-		// Instantiate global variables
-	}
-	
-	public int getID() {
-		return ID;
-	}
+    private int ID;
+    private int duration;
+    private Double price;
+    private LocalDateTime startDateTime;
 
-	public void setID(int iD) {
-		ID = iD;
-	}
-	
-	public int getDuration() {
-		return duration;
-	}
 
-	public void setDuration(int duration) {
-		this.duration = duration;
-	}
+    public Performance(String date, int duration) {
+        this.setDuration(duration);
+        this.startDateTime = LocalDateTime.parse(date);
+        // Instantiate global variables
+    }
 
-	public Double getPrice() {
-		return price;
-	}
+    public int getID() {
+        return ID;
+    }
 
-	public void setPrice(Double price) {
-		this.price = price;
-	}
+    public void setID(int iD) {
+        ID = iD;
+    }
 
-	public LocalDateTime getStartDateTime() {
-		return startDateTime;
-	}
+    public int getDuration() {
+        return duration;
+    }
 
-	public void setStartDateTime(LocalDateTime startDateTime) {
-		DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(startDateTime);
-		this.startDateTime = startDateTime;
-	}
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(LocalDateTime startDateTime) {
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(startDateTime);
+        this.startDateTime = startDateTime;
+    }
 }
