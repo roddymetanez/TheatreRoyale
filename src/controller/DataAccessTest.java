b7baeb37bc251ad80d6babc1f55ec1d5666c5b8e
RosiesMum <101412467+RosiesMum@users.noreply.github.com>
2022-03-16 16:19:25 +0000

    First pass with test data
    TestTheatre.sql for simple DB with stored procedures - until we have access to the real thing.
    Guess at 3 methods to provide data  for front-end - until we have verification of what is actually required.

---
+++ src/controller/DataAccessTest.java
@@ -0,0 +1,98 @@
+package src.controller;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import src.util.DBConnector;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+/**
+ * The test class DataAccessTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class DataAccessTest
+{
+    /**
+     * Default constructor for test class DataAccessTest
+     */
+    public DataAccessTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void constructorTest() {
+        DataAccess dataAccess = new DataAccess();
+        DBConnector db = dataAccess.getDb();
+        assertNotNull(db);
+        System.out.println(db);
+
+    }
+    
+    @Test
+    public void getShowsTest() {
+        DataAccess dataAccess = new DataAccess();
+        DBConnector db = dataAccess.getDb();
+        ResultSet rs = dataAccess.getShows();
+        assertNotNull(rs);
+        db.printResult(rs);
+    }
+    
+    @Test
+    public void getShowByNameTest() {
+        DataAccess dataAccess = new DataAccess();
+        DBConnector db = dataAccess.getDb();
+        ResultSet rs = dataAccess.getShowByName("Alice");
+        assertNotNull(rs);
+        db.printResult(rs);
+    }
+    
+    @Test
+    public void getShowByDateTest() {
+        DataAccess dataAccess = new DataAccess();
+        DBConnector db = dataAccess.getDb();
+        ResultSet rs = dataAccess.getShowByDate("01-04-22");
+        assertNotNull(rs);
+        db.printResult(rs);
+    }
+    
+    @Test
+    public void closeTest() {
+        DataAccess dataAccess = new DataAccess();
+        dataAccess.close();
+        Connection conn = dataAccess.getDb().getConn();
+        try {
+            assertTrue(conn.isClosed());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
