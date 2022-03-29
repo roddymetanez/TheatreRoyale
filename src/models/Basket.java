51fb83278de5e8722c972df30ae79f19bfb9b228
Rory MacTague <70365402+roddymetanez@users.noreply.github.com>
2022-03-29 15:00:43 +0400

    To Match #35
    To Match #35

--- src/models/Basket.java
+++ src/models/Basket.java
@@ -20,9 +20,14 @@ public class Basket {
 		ticket.computeCost(ticketsInBasket);
 	}
 
-	public boolean removeFromBasket(Ticket ticket) {
-		return ticketsInBasket.remove(ticket);
-	}
+	public boolean removeFromBasket(int perfID) {
+ 		for (Ticket ticket : ticketsInBasket) {
+ 			if (ticket.getPerformanceID() == perfID) {
+ 				ticketsInBasket.remove(ticket);
+ 				return true;
+ 			}
+ 		}
+ 		return false;
 
 	public double getBasketTotalCost() {
 		double total = 0;
